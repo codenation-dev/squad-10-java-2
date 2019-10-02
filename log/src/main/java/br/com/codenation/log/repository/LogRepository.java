eaa4439e5ad51704b226cbca05844f168e36a3b8
Gabriel Bohrer Schmitt <gabrielbs@dbserver.com.br>
2019-10-02 08:48:26 -0300

    Corrige ambiguidade no endpoint e imports no repository

--- log/src/main/java/br/com/codenation/log/repository/LogRepository.java
+++ log/src/main/java/br/com/codenation/log/repository/LogRepository.java
@@ -1,8 +1,8 @@
 package br.com.codenation.log.repository;
 
 import br.com.codenation.log.entity.Log;
-import br.com.codenation.log.entity.enums.Ambiente;
-import br.com.codenation.log.entity.enums.Nivel;
+import br.com.codenation.log.enums.Ambiente;
+import br.com.codenation.log.enums.Nivel;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,7 +20,7 @@ public interface LogRepository extends JpaRepository<Log, Long> {
 
     List<Log> findAllByAmbienteAndDescricao(Ambiente ambiente, String decricao, Sort sort);
 
-    @Query(value="SELECT * FROM log WHERE ambiente=?1 AND payload ->> 'origem'= ?2 ORDER BY ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM log WHERE ambiente=?1 AND payload ->> 'origem'= ?2 ORDER BY ?3", nativeQuery = true)
     List<Log> findAllByAmbienteAndOrigem(Ambiente ambiente, String origem, String ordenacao);
 
     void deleteByAmbienteAndId(Ambiente ambiente, Long id);
