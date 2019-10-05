a178e6c1ffe8906792e262c08a218fa3d125ac39
Wilson Ferreira <wilson_tama@hotmail.com>
2019-10-04 23:18:38 -0300

    Removendo arquivos que não serão mais utilizados

--- agent/src/main/java/br/com/codenation/agent/enums/Ambiente.java
+++
@@ -1,5 +0,0 @@
-package br.com.codenation.agent.enums;
-
-public enum Ambiente {
-    PRODUCAO, HOMOLOGACAO, DESENVOLVIMENTO
-}
--- agent/src/main/java/br/com/codenation/agent/enums/Nivel.java
+++
@@ -1,5 +0,0 @@
-package br.com.codenation.agent.enums;
-
-public enum Nivel {
-    DEBUG, WARNING, ERROR
-}
